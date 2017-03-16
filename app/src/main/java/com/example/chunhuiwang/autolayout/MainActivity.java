c17ab9da71d824d2290a058398a5adad9ddd3a5d
汪春晖 <chunhuiwang@Ctrip.com>
2017-03-16 17:17:23 +0800

    vvv

---
+++ app/src/main/java/com/example/chunhuiwang/autolayout/MainActivity.java
@@ -0,0 +1,20 @@
+package com.example.chunhuiwang.autolayout;
+
+import android.os.Build;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Toast;
+
+import java.util.HashMap;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Toast.makeText(this, Build.VERSION.RELEASE,Toast.LENGTH_LONG).show();
+    }
+}
